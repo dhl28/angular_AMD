ab000676d319470c27cb15252c90b238ea17c5f4
dhl28 <dhl28@126.com>
2016-09-23 16:56:43 +0800

    add themes files

---
+++ public/javascripts/directive/sidebar.directive.js
@@ -0,0 +1,18 @@
+/**
+ * Created by dhl on 2016/9/23.
+ */
+define(['angularAMD'], function (angularAMD) {
+    angularAMD.directive('sideBar', [ function () {
+        return {
+            require: '?ngModel',
+            // can be used as attribute or element
+            restrict: 'AE',
+            // which markup this directive generates
+            templateUrl:'/public/html/layout/sidebar.html',
+            scope: {},
+            link: function (scope, element, attrs, controller) {
+                console.log('sidebar init')
+            }
+        };
+    }])
+});
